3d2786063e314713e49cd0e9e97609e4fbbe9e50
AndeyGordeev <prolodgy@yandex.ru>
2017-12-04 21:29:00 +0300

    Динамическое содержимое

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from book import views
 
 urlpatterns = [
     url(r'^hello/$', views.hello, name='hello'),
+    url(r'^time/$', views.current_datetime, name='time'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime
 # Create your views here.
 
 def hello(request):
     return HttpResponse('Hello world!')
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html><body>It is now %s.' % now
+    return HttpResponse(html)
