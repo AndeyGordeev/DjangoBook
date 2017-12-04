483d54b415531215c7bfe3f9045bc233b2d6885d
AndeyGordeev <prolodgy@yandex.ru>
2017-12-05 02:44:05 +0300

    Подключена MySql

--- DjangoBook/settings.py
+++ DjangoBook/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoBook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdatabasee',
+        'USER': 'root',
+        'PASSWORD': 'moyazayaka21juliazayac21',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
