7424650dab169aa8010f17959c7aa5bb2f48baec
AndeyGordeev <prolodgy@yandex.ru>
2017-12-04 21:49:45 +0300

    Динамическое url

--- book/urls.py
+++ book/urls.py
@@ -5,4 +5,6 @@ from book import views
 urlpatterns = [
     url(r'^hello/$', views.hello, name='hello'),
     url(r'^time/$', views.current_datetime, name='time'),
+    url(r'^time/plus/([0-9]{1,2})/$', views.hours_ahead, name='plustime'),
+    url(r'^another-time-page/$', views.current_datetime, name='anothertime'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime
 # Create your views here.
 
@@ -9,4 +9,13 @@ def hello(request):
 def current_datetime(request):
     now = datetime.datetime.now()
     html = '<html><body>It is now %s.' % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = '<html><body>In %s hour(s), it will be %s.</body></html>'% (offset,dt)
     return HttpResponse(html)
