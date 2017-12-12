27024a90b7fa8e7ec1c38641dc62203234a3c212
AndreyGordeev <prolodgy@yandex.ru>
2017-12-12 19:04:40 +0300

    Fields добавление и улаление свойств

--- book/admin.py
+++ book/admin.py
@@ -12,6 +12,7 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)
+    fields = ('title', 'authors', 'publisher')
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
